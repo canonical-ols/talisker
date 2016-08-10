43340b428f9a3c77285a44088608e03ce4a87a83
bloodearnest <simon.davy@canonical.com>
2016-08-10 11:33:20 +0100

    Bump version: 0.4.1 → 0.5.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ if sys.version_info < (3, 3):
 
 setup(
     name='talisker',
-    version='0.4.1',
+    version='0.5.0',
     description="A common WSGI stack",
     long_description=readme + '\n\n' + history,
     author="Simon Davy",
