a056422575464ca8f6222a3da0f3a6765e50e02a
bloodearnest <simon.davy@canonical.com>
2016-09-21 22:36:06 +0100

    Fix tests for new celery module

--- talisker/celery.py
+++ talisker/celery.py
@@ -1,6 +1,37 @@
+# Copyright (C) 2016- Canonical Ltd
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+from future import standard_library
+standard_library.install_aliases()
+from builtins import *  # noqa
+
 import functools
 
 from talisker import logs, request_id
+
+
+__all__ = [
+    'logging',
+    'delay',
+    ]
 
 
 def logging(func):
