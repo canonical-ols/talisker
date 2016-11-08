e1db5cfcf9483677ce0623398d034dae85990d72
Simon Davy <bloodearnest@gmail.com>
2016-11-08 22:48:13 +0000

    removed accidentaly added file

--- talisker/flask.py
+++
@@ -1,9 +0,0 @@
-from raven.contrib.flask import Sentry
-import talisker.raven
-
-def sentry(app):
-    return Sentry(app,
-                  client=talisker.raven.get_client(),
-                  logging=False,
-                  wrap_wsgi=None,
-                  register_signal=False)
