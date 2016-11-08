9ea0d5d77657a8416fe7d677a3ad9822b49e3865
Simon Davy <bloodearnest@gmail.com>
2016-11-08 22:45:37 +0000

    remove future's stdlib install hooks, as they break some vendored deps libraries use

--- talisker/requests.py
+++ talisker/requests.py
@@ -19,8 +19,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from future import standard_library
-standard_library.install_aliases()
 from builtins import *  # noqa
 
 import logging
@@ -28,7 +26,6 @@ import threading
 import requests
 import functools
 from requests.utils import to_native_string
-
 from . import statsd
 from .util import parse_url
 from . import request_id
--- talisker/util.py
+++ talisker/util.py
@@ -19,11 +19,10 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-from future import standard_library
-standard_library.install_aliases()
 from builtins import *  # noqa
 
-from urllib.parse import urlparse
+
+from future.moves.urllib.parse import urlparse
 
 
 def parse_url(url, proto='http'):
