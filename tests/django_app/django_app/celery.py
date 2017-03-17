001770e6840c72db1d85fecadd4442e94afe142c
bloodearnest <bloodearnest@gmail.com>
2017-03-17 09:20:39 +0000

    Add limbo to test minimum versions

--- tests/django_app/django_app/celery.py
+++ tests/django_app/django_app/celery.py
@@ -12,10 +12,10 @@ app = Celery('django_app')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.config_from_object('django.conf:settings')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks('django_app')
 
 
 @app.task(bind=True)
