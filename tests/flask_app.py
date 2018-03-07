858528ee1a5153c067328ef398c422252bbbdb14
Simon Davy <bloodearnest@gmail.com>
2018-03-07 07:50:30 +0000

    Fix some small logging errors (#142)
    * clean up a few small error handling and logging issues
    * rework tests work better offline

--- tests/flask_app.py
+++ tests/flask_app.py
@@ -1,11 +1,13 @@
+import logging
+
 from flask import Flask
 import sqlalchemy
 from sqlalchemy import Table, Column, Integer, String, MetaData, select
 
-
 import talisker.flask
 from talisker.postgresql import TaliskerConnection
 
+logger = logging.getLogger(__name__)
 engine = sqlalchemy.create_engine(
     'postgresql://django_app:django_app@localhost:5432/django_app',
     connect_args={'connection_factory': TaliskerConnection},
@@ -23,18 +25,20 @@ users = Table(
 metadata.create_all(engine)
 conn = engine.connect()
 conn.execute(users.insert().values(name='jack', fullname='Jack Jones'))
-conn.execute(select([users]))
 
 
 app = Flask(__name__)
 talisker.flask.register(app)
 
-talisker.requests.get_session().post(
-    'http://httpbin.org/post', json={'foo': 'bar'})
-
 
 @app.route('/')
 def index():
+    return 'ok'
+
+
+@app.route('/logging')
+def logging():
+    logger.info('info', extra={'foo': 'bar'})
     talisker.requests.get_session().post(
         'http://httpbin.org/post', json={'foo': 'bar'})
     return 'ok'
