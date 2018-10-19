0de0d1b27cc80b86f875459f1dd84a7bb199770d
Simon Davy <simon.davy@canonical.com>
2018-10-19 14:20:25 +0000

    fix for older raven

--- talisker/sentry.py
+++ talisker/sentry.py
@@ -155,9 +155,10 @@ def add_talisker_context(data):
         data['tags']['request_id'] = rid
     data['extra'].update(talisker.logs.logging_context.flat)
 
-    if 'user' in data:
-        data['user'].pop('email', None)
-        data['user'].pop('username', None)
+    user = data.get('user')
+    if user:
+        user.pop('email', None)
+        user.pop('username', None)
 
     breadcrumbs = data.get('breadcrumbs', {}).get('values', [])
     start_time = data['extra'].get('start_time')
