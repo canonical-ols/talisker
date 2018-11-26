06bb794d0d9cc90ac38c56f2a4c488c18f55415a
Simon Davy <simon.davy@canonical.com>
2018-11-26 17:02:43 +0000

    Bump version: 0.10.0 → 0.10.1

--- docs/conf.py
+++ docs/conf.py
@@ -72,7 +72,7 @@ copyright = u'2016, Canonical Ltd'
 # for |version| and |release|, also used in various other places throughout
 # the built documents.
 # The short X.Y version.
-version = '0.10.0'
+version = '0.10.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
