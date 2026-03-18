f1fbbb46ee51cb041910d35997a6f1d9ca81beca
Maximiliano Bertacchini <maxiberta@gmail.com>
2026-03-18 13:49:57 +0000

    Drop python 3.5 & 3.6 support

--- setup.py
+++ setup.py
@@ -114,8 +114,6 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: WSGI',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
         'Topic :: System :: Logging',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.12',
